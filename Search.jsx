388584ed5437721f6ff30bb151c9e93cb1c0a058
Mr. Code <144205208+Rashed12399@users.noreply.github.com>
2024-04-06 10:47:06 +0400

    Create Search.jsx

---
+++ Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchComp(){
+    return(
+        <section>
+            <p>Search here</p>
+            <input type="text" />
+        </section>
+    );
+}
+export default SearchComp
